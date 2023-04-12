82263d2d2dc1d0b13571bff7cc39c7f8f80815b0
tungdeptrai0000@outlook.com <tungdeptrai0000@outlook.com>
2023-04-12 10:31:30 +0700

    Update: Thêm nút "Thêm mới", Form add, js ẩn hiện form add

--- PMStudent/PMLecture/wwwroot/js/site.js
+++ PMStudent/PMLecture/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const button = document.getElementById("btn-add");
+button.addEventListener("click", function () {
+    $("#form_add").toggleClass("d-none");
+    $("#from_search").toggleClass("d-none");
+    $(this)[0].innerText = ($(this)[0].innerText == "THÊM MỚI" ? "Quay lại" : "Thêm mới");
+});
