4171d72fcfe6bd4ed9fb99d015f66300be06931e
tungdeptrai0000@outlook.com <tungdeptrai0000@outlook.com>
2023-05-16 08:22:32 +0700

    Update: Chức năng cập nhật nhân viên phòng đào tạo

--- PMStudent/PMLecture/Context/GiangVienContext.cs
+++ PMStudent/PMLecture/Context/GiangVienContext.cs
@@ -243,7 +243,40 @@ namespace PMLecture.Context
 
         public CResponseMessage UpdateGiangVien(GiangVienViewModel giangVien)
         {
-            return new CResponseMessage();
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
+            IConfiguration config = builder.Build();
+            string connectionString = config.GetValue<string>("ConnectionStrings:DefaultConnection");
+
+            try
+            {
+                CResponseMessage resMess = new CResponseMessage();
+
+                var sqlcon = DBConnection.GetSqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("GV_SP_UpdateEmployee", sqlcon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MaGiangVien", giangVien.MaGiangVien);
+                cmd.Parameters.AddWithValue("@HoTen", giangVien.HoTen);
+                cmd.Parameters.AddWithValue("@GioiTinh", giangVien.GioiTinh);
+                cmd.Parameters.AddWithValue("@TrinhDo", (object)giangVien.TrinhDo ?? DBNull.Value);
+
+                cmd.Parameters.Add("@Code", SqlDbType.NVarChar, 100);
+                cmd.Parameters["@Code"].Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("@Message", SqlDbType.NVarChar, 100);
+                cmd.Parameters["@Message"].Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("@Data", SqlDbType.NVarChar, 100);
+                cmd.Parameters["@Data"].Direction = ParameterDirection.Output;
+
+                var reader = cmd.ExecuteNonQuery();
+                resMess.Code = Convert.ToInt32(cmd.Parameters["@Code"].Value);
+                resMess.Message = Convert.ToString(cmd.Parameters["@Message"].Value);
+                resMess.Data = Convert.ToString(cmd.Parameters["@Data"].Value);
+
+                return resMess;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public CResponseMessage DeleteGiangVien(string maGiangVien)
